a67439873aed829b29de3b7f0b47c29b9edc6aac
algorythmik <smt.moji@gmail.com>
2024-09-07 17:59:21 +0200

    make mvp for linear regression header

--- include/xtensor_ml/models/linear/linear_regression.hpp
+++ include/xtensor_ml/models/linear/linear_regression.hpp
@@ -7,25 +7,19 @@ namespace linear_model {
 class LinearRregression {
     public:
         // constructor
-        explicit LinearRregression(bool fit_intercept=true);
+        explicit LinearRregression();
 
         // fit method to train the model
-            LinearRregression& fit(
-                const xt::xarray<double>& X,
-                const xt::xarray<double>& y,
-                const xt::xarray<double>* weights = nullptr
-              );
+        LinearRregression& fit(
+            const xt::xarray<double>& X,
+            const xt::xarray<double>& y
+        );
 
-            xt::xarray<double> predict(const xt::xarray<double>& X) const;
+          xt::xarray<double> predict(const xt::xarray<double>& X) const;
 
     private:
-        bool fit_intercept_;
         xt::xarray<double> beta_;
-        xt::xarray<double> sigma_inv_;
         bool is_fit_;
-
-    void update_1d(const xt::xarray<double>& X, const xt::xarray<double>& y, const xt::xarray<double>& w);
-    void update_2d(const xt::xarray<double>& X, const xt::xarray<double>& y, const xt::xarray<double>& w);
 
 };
 }
