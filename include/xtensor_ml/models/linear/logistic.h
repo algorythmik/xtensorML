82ddab580d8d0f6630bbb3a06e27705b86ce7156
algorythmik <smt.moji@gmail.com>
2024-09-15 09:29:09 +0200

    Add header file for logistic

---
+++ include/xtensor_ml/models/linear/logistic.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "xtensor/xtensor_forward.hpp"
+#include <xtensor/xarray.hpp>
+namespace xtensor_ml{
+namespace linear_models{
+} // end of linear_modles namespace
+enum class Penalty{
+    l1,
+    l2
+};
+
+class LogisticRegression {
+    public:
+        explicit LogisticRegression(double gamma, Penalty penalty, bool fit_intercept=true);
+
+        LogisticRegression& fit(
+            const xt::xarray<double>& X,
+            const xt::xarray<double>& y
+        );
+
+        xt::xarray<double> predict(const xt::xarray<double>& X) const;
+        xt::xarray<double> get_coefs() const;
+
+    private:
+        double gamma_;
+        bool fit_intercept_;
+        Penalty penalty_;
+
+        double NLL_(
+                const xt::xarray<double>& y,
+                const xt::xarray<double>& y_pred
+        ) const;
+        xt::xarray<double> NLL_grad_(
+                const xt::xarray<double>& X,
+                const xt::xarray<double>& y,
+                const xt::xarray<double>& Y_pred
+        ) const;
+        static xt::xarray<double> sigmoid_(
+                const xt::xarray<double>& z);
+
+
+};
+
+} // end of xtensor_ml
