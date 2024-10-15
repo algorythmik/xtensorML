068e629ccd0a7338f001287e8743fc479a784e81
algorythmik <smt.moji@gmail.com>
2024-10-15 20:02:19 +0200

    dt implementaiton, wip

--- include/xtensor_ml/trees/dt.hpp
+++ include/xtensor_ml/trees/dt.hpp
@@ -1,18 +1,13 @@
 #pragma once
 
 #include "xtensor/xtensor_forward.hpp"
+#include <cstddef>
 #include <memory>
 #include <stdlib.h>
 #include <xtensor/xarray.hpp>
 using xt::xarray;
 namespace xtensor_ml {
 namespace trees {
-class Leaf {
-
-public:
-  xarray<double> value;
-  Leaf(xarray<double> &value_) : value(value_) {};
-};
 class Node {
 public:
   std::shared_ptr<Node> left;
@@ -27,19 +22,46 @@ public:
         threshold(threshold) {};
 };
 
+class Leaf : public Node {
+
+public:
+  xarray<double> value;
+  explicit Leaf(xarray<double> &value_)
+      : Node(nullptr, nullptr, -1, 0), value(value_) {};
+};
+enum class Criterion {
+  gini,
+  entropy,
+};
+
 // for now classifier is always is assumed to be true,
 class DecisionTree {
 public:
-  DecisionTree(bool classifier, int max_depth) : max_depth(max_depth) {};
+  explicit DecisionTree(int max_depth = 100,
+                        Criterion criterion = Criterion::gini);
   DecisionTree &fit(const xarray<double> &X, const xarray<double> &y);
   xarray<double> predict(const xarray<double> &X);
 
 private:
-  std::shared_ptr<Node> root;
-  int max_depth;
+  std::shared_ptr<Node> root_;
+  int max_depth_;
+  Criterion criterion_;
+  size_t depth_;
+
+  /// Recursively grow the decison tree.
+  std::shared_ptr<Node> Grow(const xarray<double> &X, const xarray<int> &y,
+                             size_t cur_depth);
+  /// Finds the best feature and trheshold to split the data
+  std::pair<size_t, double>
+  Segment(const xarray<double> &X, const xarray<int> &y, const xarray<size_t>   &feat_id);
+  /// Calculates the impurity gain of a split
+  double ImputiryGain(const xarray<double> &y, double split_thresh,
+                      const xarray<double> &feat_values);
+  double Impurity(const xarray<int> &y);
+  int Traverse(const xarray<double> &X, const std::shared_ptr<Node> &node);
 };
 
-double entropy(const xarray<int> &y);
-double gini(const xarray<int> &y);
+double Entropy(const xarray<int> &y);
+double Gini(const xarray<int> &y);
 } // namespace trees
 } // namespace xtensor_ml
--- src/models/trees/dt.cpp
+++ src/models/trees/dt.cpp
@@ -3,16 +3,68 @@
 #include "xtensor/xhistogram.hpp"
 #include "xtensor/xio.hpp"
 #include "xtensor/xmath.hpp"
+#include "xtensor/xoperation.hpp"
+#include "xtensor/xrandom.hpp"
+#include "xtensor/xsort.hpp"
+#include "xtensor/xtensor.hpp"
 #include "xtensor/xtensor_forward.hpp"
+#include <cstddef>
+#include <limits>
+#include <memory>
 // using xtensor_ml::trees::DecisionTree;
 using xt::xarray;
 namespace xtensor_ml {
 namespace trees {
 
-// DecisionTree& fit(const xarray<double> &X, const xarray<double>& y){
-// };
+DecisionTree::DecisionTree(int max_depth, Criterion criterion)
+    : max_depth_(max_depth), criterion_(criterion), root_(nullptr),
+      depth_(0) {};
+DecisionTree &DecisionTree::fit(const xarray<double> &X,
+                                const xarray<double> &y) {
+  root_ = Grow(X, y, 0);
+  return *this;
+};
 
-double entropy(const xarray<int> &y) {
+std::shared_ptr<Node> DecisionTree::Grow(const xarray<double> &X,
+                                         const xarray<int> &y,
+                                         size_t cur_depth) {
+  if (xt::unique(y).size() == 1 || cur_depth >= max_depth_) {
+    xarray<double> proba = xt::bincount(y) / static_cast<double>(y.size());
+    return std::make_shared<Leaf>(proba);
+  }
+  cur_depth++;
+  depth_ = std::max(cur_depth, depth_);
+  xarray<size_t> feat_idxs =
+      xt::random::choice(xt::arange<size_t>(X.shape(1)), X.shape(1), false)();
+  auto result = Segment(X, y, feat_idxs);
+  size_t best_feat = result.first;
+  double best_thresh = result.second;
+  auto feat_values = xt::view(X, xt::all(), best_feat);
+  auto left_idx = xt::flatten_indices(xt::where(feat_values <= best_thresh));
+  auto right_idx = xt::flatten_indices(xt::where(feat_values > best_thresh));
+  auto left = Grow(xt::view(X, left_idx), xt::view(y, left_idx), cur_depth);
+  auto right = Grow(xt::view(X, right_idx), xt::view(y, right_idx), cur_depth);
+  return std::make_shared<Node>(left, right, best_feat, best_thresh);
+};
+
+std::pair<size_t, double> Segment(const xarray<double> &X, const xarray<int> &y,
+                                  const xarray<size_t> &feat_idxs) {
+  double best_gain = std::numeric_limits<double>::infinity();
+  int best_feat = -1;
+  double best_thresh = 0.0;
+
+  for (size_t feat_idx : feat_idxs) {
+    xarray<double> vals = xt::view(X, xt::all(), feat_idx);
+    xarray<double> levels = xt::unique(vals);
+    xarray<double> thresholds =
+        (xt::view(levels, xt::range(0, -1))) +
+        xt::view(levels, xt::range(1, levels.size() + 1)) / 2.0;
+  }
+  for (double tr : thresholds) {
+  }
+};
+
+double Entropy(const xarray<int> &y) {
   xarray<double> bin_count = xt::bincount(y);
   xarray<double> total = xt::sum(bin_count);
   xarray<double> proba = bin_count / total;
@@ -20,7 +72,7 @@ double entropy(const xarray<int> &y) {
   auto values = xt::where(proba > 0, -proba * log_probs, 0);
   return xt::sum(values)();
 }
-double gini(const xarray<int> &y) {
+double Gini(const xarray<int> &y) {
   // Gini impurity measure
   xarray<double> bin_count = xt::bincount(y);
   xarray<double> total = xt::sum(bin_count);
